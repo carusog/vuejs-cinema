17c3458568aa59759bf6d9469eb780471fa135ad
Giuseppe Caruso <giuseppe@bonsai-studio.net>
2018-04-01 17:30:55 +0200

    Project 2 - Setting up components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,52 @@
 import Vue from 'vue';
 import './style.scss';
 
+import genres from './util/genres';
+
 new Vue({
     el: '#app',
-    data: {
-        msg: 'Hola mundo!'
+    components: {
+        'movie-list': {
+            template: `
+                <div id="movie-list">
+                    <div
+                        v-for="movie in movies"
+                        class="movie"
+                    >
+                        <h4>{{movie.title}}</h4>
+                    </div>
+                </div>
+            `,
+            data() {
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Home Alone' },
+                        { title: 'Austin Powers' },
+                    ]
+                }
+            }
+        },
+        'movie-filter': {
+            data() {
+                return {
+                    genres
+                }
+            },
+            template: `
+                <div id="movie-filter">
+                    <h2>Filter results</h2>
+                    <div class="filter-group">
+                        <check-filter v-for="genre in genres"/>
+                    </div>
+                    <check-filter/>
+                </div>
+            `,
+            components: {
+                'check-filter': {
+                    template: `<div>Filter</div>`
+                }
+            }
+        }
     }
 })

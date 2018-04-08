a5eb4de54a5c21064db7be8eb4e20ecfa12bead1
Giuseppe Caruso <giuseppe@bonsai-studio.net>
2018-04-08 15:07:13 +0200

    Project 2 - Refactoring with single file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="movie-list">
+        <div v-for="movie in filteredMovies" :key="movie.title" class="movie">
+            {{movie.title}}
+        </div>
+    </div>
+</template>
+<script>
+
+import genres from '../util/genres.js';
+
+export default {
+    data() {
+        return {
+            movies: [
+                { title: "Kill Bill", genre: genres.CRIME },
+                { title: "Home Alone", genre: genres.COMEDY },
+                { title: "Austin Powers", genre: genres.COMEDY }
+            ]
+        };
+    },
+    props: ["genre", "time"],
+    methods: {
+        moviePassesGenreFilter(movie) {
+        return this.genre.length
+            ? this.genre.find(genre => movie.genre === genre)
+            : true;
+        }
+    },
+    computed: {
+        filteredMovies() {
+            return this.movies.filter(this.moviePassesGenreFilter);
+        }
+    }
+};
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import './style.scss';
 
-import genres from './util/genres';
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
     el: '#app',
@@ -11,94 +12,20 @@ new Vue({
     },
     methods: {
         checkFilter(category, title, checked) {
-            console.log(category, title, checked);
-            checked ?
-                this[category].push(title) :
-                this[category].indexOf(title) >= 0 ?
-                    this[category].splice(this[category].indexOf(title), 1) :
-                    null;
+
+            let titleIndex = this[category].indexOf(title);
+
+            let removeCategoryOfNull = titleIndex >= 0
+                ? this[category].splice(titleIndex, 1)
+                : null;
+
+            checked
+                ? this[category].push(title)
+                : removeCategoryOfNull;
         }
     },
     components: {
-        'movie-list': {
-            template: `
-                <div id="movie-list">
-                    <div v-for="movie in filteredMovies" class="movie">
-                        {{movie.title}}
-                    </div>
-                </div>
-            `,
-            data() {
-                return {
-                    movies: [
-                        { title: 'Pulp Fiction', genre: genres.CRIME },
-                        { title: 'Home Alone', genre: genres.COMEDY },
-                        { title: 'Austin Powers', genre: genres.COMEDY }
-                    ]
-                }
-            },
-            props: [ 'genre', 'time' ],
-            methods: {
-                moviePassesGenreFilter(movie) {
-                    return this.genre.length ?
-                        this.genre.find(genre => movie.genre === genre) :
-                        true;
-                }
-            },
-            computed: {
-                filteredMovies() {
-                    return this.movies.filter(this.moviePassesGenreFilter);
-                }
-            }
-        },
-        'movie-filter': {
-            data() {
-                return {
-                    genres
-                }
-            },
-            methods: {
-                checkFilter(category, title, checked) {
-                    this.$emit('check-filter', category, title, checked);
-                }
-            },
-            template: `
-                <div id="movie-filter">
-                    <h2>Filter results</h2>
-                    <div class="filter-group">
-                        <check-filter
-                            v-for="genre in genres"
-                            v-bind:title="genre"
-                            v-on:check-filter="checkFilter"
-                        ></check-filter>
-                    </div>
-                </div>
-            `,
-            components: {
-                'check-filter': {
-                    template: `
-                        <div
-                            v-bind:class="{ 'check-filter': true, active: checked }"
-                            v-on:click="checkFilter"
-                        >
-                            <span class="checkbox"></span>
-                            <span class="check-filter-title">{{ title }}</span>
-                        </div>
-                    `,
-                    props: ['title'],
-                    data() {
-                        return {
-                            checked: false
-                        }
-                    },
-                    methods: {
-                        checkFilter() {
-                            this.checked = !this.checked;
-                            this.$emit('check-filter', 'genre', this.title, this.checked);
-                        }
-                    }
-                }
-            }
-        }
+        MovieList,
+        MovieFilter
     }
 })
